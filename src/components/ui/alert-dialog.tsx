f289ba231bbbc22d8b9d021c429b6f1d39f47db2
Adeonir Kohl <adeonir@gmail.com>
2026-09-12 00:10:48 -0300

    chore: upgrade dependencies and rework the pull request pipeline (#35)

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -109,14 +109,14 @@ function AlertDialogCancel({ className, ...props }: React.ComponentProps<typeof 
 
 export {
   AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 }
